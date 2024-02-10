76b1746dd85fdbe99778c65d93add3f73b3807e5
Santana George <santanageorge@outlook.com>
2024-02-10 09:09:12 +0530

    sa

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
